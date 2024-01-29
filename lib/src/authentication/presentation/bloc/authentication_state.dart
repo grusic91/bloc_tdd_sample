24782f9a80dc71bc6404c485bda491842a7ee95e
Grus <gasper.rus@programmfabrik.de>
2024-01-29 11:00:39 +0100

    handle authentication state with bloc.

--- lib/src/authentication/presentation/bloc/authentication_state.dart
+++ lib/src/authentication/presentation/bloc/authentication_state.dart
@@ -7,4 +7,36 @@ sealed class AuthenticationState extends Equatable {
   List<Object> get props => [];
 }
 
-final class AuthenticationInitial extends AuthenticationState {}
+final class AuthenticationInitial extends AuthenticationState {
+  const AuthenticationInitial();
+}
+
+class CreatingUser extends AuthenticationState {
+  const CreatingUser();
+}
+
+class GettingUsers extends AuthenticationState {
+  const GettingUsers();
+}
+
+class UserCreated extends AuthenticationState {
+  const UserCreated();
+}
+
+class UsersLoaded extends AuthenticationState {
+  const UsersLoaded(this.users);
+
+  final List<User> users;
+
+  @override
+  List<Object> get props => users.map((user) => user.id).toList();
+}
+
+class AuthenticationError extends AuthenticationState {
+  const AuthenticationError(this.message);
+
+  final String message;
+
+  @override
+  List<String> get props => [message];
+}
