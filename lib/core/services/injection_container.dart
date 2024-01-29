09b5cd8b655d96ef7b4552e72c4d1c986d0bbddb
Grus <gasper.rus@programmfabrik.de>
2024-01-29 16:59:56 +0100

    implement dependencie injection container.

---
+++ lib/core/services/injection_container.dart
@@ -0,0 +1,34 @@
+import 'package:bloc_tdd_sample/src/authentication/data/datasources/authentication_remote_data_source.dart';
+import 'package:bloc_tdd_sample/src/authentication/data/repositories/authentication_repository_implementation.dart';
+import 'package:bloc_tdd_sample/src/authentication/domain/repositories/authentiaction_repository.dart';
+import 'package:bloc_tdd_sample/src/authentication/domain/usecases/create_user.dart';
+import 'package:bloc_tdd_sample/src/authentication/domain/usecases/get_users.dart';
+import 'package:bloc_tdd_sample/src/authentication/presentation/cubit/authentication_cubit.dart';
+import 'package:get_it/get_it.dart';
+import 'package:http/http.dart' as http;
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  // App Logic
+  sl
+    ..registerFactory(() => AuthenticationCubit(
+          createUser: sl(),
+          getUsers: sl(),
+        ))
+
+    // Use Cases
+    ..registerLazySingleton(() => CreateUser(sl()))
+    ..registerLazySingleton(() => GetUsers(sl()))
+
+    // Reposiotries
+    ..registerLazySingleton<AuthoenticationRepository>(
+        () => AuthenticationRepositoryImplementation(sl()))
+
+    // Data Sources
+    ..registerLazySingleton<AuthenticationRemoteDataSource>(
+        () => AuthRemoteDataSrcImpl(sl()))
+
+    // External dependecies
+    ..registerLazySingleton(() => http.Client.new);
+}
